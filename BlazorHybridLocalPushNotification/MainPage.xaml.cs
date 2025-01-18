86d7b5d0874fc00c95d7b9339cb773abad29be40
ganeshpaiya <ganesanhere4u@gmail.com>
2025-01-18 20:28:44 +0530

    Add project files.

---
+++ BlazorHybridLocalPushNotification/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorHybridLocalPushNotification
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
